e02a49f172a828e583b1bc56aa2818186d9fd6a8
Lucas <lucasgaarciaa@gmail.com>
2019-09-14 16:17:52 -0300

    merge

--- scripts-blog.js
+++ scripts-blog.js
@@ -8,19 +8,17 @@ $.get('https://blog.woodprime.com.br/wp-json/wp/v2/posts?filter[orderby]=date&pe
               "Agosto", "Setembro", "Outubro",
               "Novembro", "Dezembro"
             ];
-          
             var day = date.getDate(date);
             var monthIndex = date.getMonth(date);
             var year = date.getFullYear(date);
-          
             return day + ' de ' + monthNames[monthIndex] + ' de ' + year;
-          }
+        }
         var image = i._embedded['wp:featuredmedia'][0].source_url;
         var title = i.title.rendered;
         var description = i.excerpt.rendered;
         var link = i.link; 
         var html ='<li class="list__post-item">' +
-            '<div class="list__post-thumbnail"><img src="'+ image + '" alt="' + title + '" title="' + title + '"/></div>' +
+            '<div class="list__post-thumbnail"><a class="list__post-link" target="_blank" href="' +link + '"><img src="'+ image + '" alt="' + title + '" title="' + title + '"/></a></div>' +
             '<div class="list__post-content">' +
                 '<span class="list__post-title">' + title + '</span>' +
                 '<span class="list__post-data">' + formatDate(new Date(date))  + '</span>' +
@@ -28,6 +26,21 @@ $.get('https://blog.woodprime.com.br/wp-json/wp/v2/posts?filter[orderby]=date&pe
                 '<a class="list__post-link" target="_blank" href="' +link + '">Continuar lendo...</a>' +
             '</div>' +
         '</li>';
-        $('.list__post').append($(html));
+        $('ul.list__post').append($(html));
     });
 });
+ 
+setTimeout(function(){ 
+    $('ul.list__post').not('.slick-initialized').slick({
+        dots: true,
+        infinite: false, 
+        centerMode: false,
+        variableWidth: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 300,
+        arrows: false
+    });
+    $(window).trigger('resize');
+},2000);
+ 
